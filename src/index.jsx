023de26dacaccb86718082b5f09b7fef7b4dd1c6
Joris Bertier <joris.bertier@gmail.com>
2024-08-26 15:08:00 -0400

    add useContext & drakmode

--- src/index.jsx
+++ src/index.jsx
@@ -6,27 +6,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/index.jsx'
 import Error from './components/Error/index.jsx'
 import Freelances from './pages/Freelances/index.jsx'
-import { createGlobalStyle } from 'styled-components'
+import Footer from './components/Footer/index.jsx'
+import { ThemeProvider } from './utils/context/index.jsx'
+import GlobalStyle from './utils/style/GlobalStyle.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
 
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/survey/:questionNumber" element={<Survey/>}/>
-        <Route path="/freelances" element={<Freelances/>}/>
-        <Route path="*" element={<Error/>}/>
-      </Routes>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/survey/:questionNumber" element={<Survey/>}/>
+          <Route path="/freelances" element={<Freelances/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+        <Footer/>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
