d4093d73376006a65861f4163e0f319b6bf267ec
Joris Bertier <joris.bertier@gmail.com>
2024-08-26 10:17:39 -0400

    add styled

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/index.jsx'
 import Error from './components/Error/index.jsx'
 import Freelances from './pages/Freelances/index.jsx'
+import { createGlobalStyle } from 'styled-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
+
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
