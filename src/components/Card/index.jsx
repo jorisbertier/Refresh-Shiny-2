64371910cce59652b32fc5925c37007614af537d
Joris Bertier <joris.bertier@gmail.com>
2024-08-26 09:47:56 -0400

    add prop types to props

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function Card({ label, title, picture }) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
@@ -6,6 +8,15 @@ function Card({ label, title, picture }) {
             <span>{title}</span>
         </div>
     )
+}
+
+Card.propTypes = {
+    label: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired
+}
+Card.defaultProps = {
+    title: 'Mon titre par défault'
 }
 
 export default Card
