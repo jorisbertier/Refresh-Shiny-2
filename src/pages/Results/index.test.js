6963ceba8f0a95f5b52b5d40be0c4d78050dc97b
Joris Bertier <joris.bertier@gmail.com>
2024-08-27 09:37:54 -0400

    convention test

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,11 +1,12 @@
 import { formatJobList } from './'
 
-describe('La fonction formatJobList', () => {
-    test('ajoute une virgule à un item', () => {
+describe('The formatJobList function', () => {
+    it('should add a comma to a word', () => {
         const expectedState = 'item2,'
         expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
     })
-    test('ne met pas de virgule pour le dernier élément', () => {
+
+    it('should not add a comma to the last element of the list', () => {
         const expectedState = 'item3'
         expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
     })
